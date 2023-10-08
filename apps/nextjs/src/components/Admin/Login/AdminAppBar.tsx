0aa01802e5a6584a914ada97691c15c7da35f012
refaelbenzvi <refael.b@utopia.tech>
2023-10-08 20:55:17 +0300

    .

--- apps/nextjs/src/components/Admin/Login/AdminAppBar.tsx
+++ apps/nextjs/src/components/Admin/Login/AdminAppBar.tsx
@@ -1,16 +1,16 @@
-import React, {ComponentProps, useState} from "react";
-import {AppBar, Modal, Row, theme, ThemeToggle, Tooltip, Typography} from "@acme/ui";
+import React, { ComponentProps, useState } from "react";
+import { AppBar, Modal, Row, theme, ThemeToggle, Tooltip, Typography } from "@acme/ui";
 import LanguageSelector from "~/components/LanguageSelector";
 import clsx from "clsx";
 import Image from "next/image";
 import useTranslation from "next-translate/useTranslation";
 import Button from "@acme/ui/src/nextjs/components/Buttons/Button";
-import {css} from "@emotion/css";
+import { css } from "@emotion/css";
 import tw from "twin.macro"
-import {signOut} from "next-auth/react";
-import {useRouter} from "next/router";
-import IconCarbonLogout from '~icons/carbon/logout'
-import IconCarbonSettings from '~icons/carbon/settings'
+import { signOut } from "next-auth/react";
+import { useRouter } from "next/router";
+import IconCarbonLogout from "~icons/carbon/logout"
+import IconCarbonSettings from "~icons/carbon/settings"
 import Settings from "~/components/Admin/Settings";
 
 export interface AdminAppBarProps extends Partial<ComponentProps<typeof AppBar>> {
@@ -19,7 +19,7 @@ export interface AdminAppBarProps extends Partial<ComponentProps<typeof AppBar>>
 }
 
 const defaultProps = {
-	removeLogoutButton: false,
+	removeLogoutButton:   false,
 	removeSettingsButton: false
 } as const
 
@@ -30,53 +30,49 @@ const AdminAppBar = (props: AdminAppBarProps) => {
 		removeSettingsButton,
 		...restProps
 	} = props
-	
-	const [isSettingsModalOpen, setIsSettingsModalOpen] = useState<boolean>(false)
-	
-	const router = useRouter()
-	const {t} = useTranslation()
-	
+
+	const [isSettingsModalOpen, setIsSettingsModalOpen] = useState<boolean> (false)
+
+	const router = useRouter ()
+	const { t } = useTranslation ()
+
 	const handleLogout = async () => {
-		await signOut({redirect: false})
-		await router.push('/admin/login')
+		await signOut ({ redirect: false })
+		await router.push ("/admin/login")
 	}
-	
+
 	const openSettings = () => {
-		setIsSettingsModalOpen(true)
+		setIsSettingsModalOpen (true)
 	}
-	
+
 	return (
 		<>
 			<Modal
 				className="my-10"
-				height={'100%'}
-				width={'500px'}
+				height={"100%"}
+				width={"500px"}
 				isOpen={isSettingsModalOpen}
 				centered
-				onBackdropClick={() => setIsSettingsModalOpen(false)}>
-				<Settings onBackButtonClick={() => setIsSettingsModalOpen(false)}/>
+				onBackdropClick={() => setIsSettingsModalOpen (false)}>
+				<Settings onBackButtonClick={() => setIsSettingsModalOpen (false)}/>
 			</Modal>
-			
+
 			<AppBar
 				{...restProps}
-				className={`justify-between px-16 max-[700px]:pl-8 max-[700px]:pr-4 ${clsx(className)}`}>
-				<Row className="items-center">
-					{/*<Image src={"/Logo.svg"} alt={'logo'} width={38} height={45}/>*/}
-				</Row>
-				
+				className={`justify-between px-16 max-[700px]:pl-8 max-[700px]:pr-4 ${clsx (className)}`}>
 				<Row className="space-s-2">
 					<Row className="ltr:pl-2 rtl:pr-2">
 						{!removeSettingsButton && (
-							<Tooltip tooltip={t('common:settings')}
+							<Tooltip tooltip={t ("common:settings")}
 							         color={theme.colorScheme.overlaysDark}
 							         placement="bottom-center">
 								<Button
 									text
 									noPadding
-									size={'22px'}
+									size={"22px"}
 									className={`${css`
-                    ${tw`p-[10px]`};
-									`} ${clsx(className)}`}
+                                      ${tw`p-[10px]`};
+									`} ${clsx (className)}`}
 									aria-label="setting"
 									id="setting-button"
 									onClick={openSettings}>
@@ -84,30 +80,30 @@ const AdminAppBar = (props: AdminAppBarProps) => {
 								</Button>
 							</Tooltip>
 						)}
-						
-						<Tooltip tooltip={t('common:language')}
+
+						<Tooltip tooltip={t ("common:language")}
 						         color={theme.colorScheme.overlaysDark}
 						         placement="bottom-center">
 							<LanguageSelector/>
 						</Tooltip>
-						
-						<Tooltip tooltip={t('common:theme')}
+
+						<Tooltip tooltip={t ("common:theme")}
 						         color={theme.colorScheme.overlaysDark}
 						         placement="bottom-center">
 							<ThemeToggle/>
 						</Tooltip>
-						
+
 						{!removeLogoutButton && (
-							<Tooltip tooltip={t('common:logout')}
+							<Tooltip tooltip={t ("common:logout")}
 							         color={theme.colorScheme.overlaysDark}
 							         placement="bottom-center">
 								<Button
 									text
 									noPadding
-									size={'22px'}
+									size={"22px"}
 									className={`${css`
-                    ${tw`p-[10px]`};
-									`} ${clsx(className)}`}
+                                      ${tw`p-[10px]`};
+									`} ${clsx (className)}`}
 									aria-label="logout"
 									id="logout-button"
 									onClick={handleLogout}>
