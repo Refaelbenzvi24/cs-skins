931bc8674718b6a1f68fb38cae80c355aec44094
refaelbenzvi <refael.b@utopia.tech>
2024-02-20 09:15:53 +0200

    test

--- apps/nextjs/src/instrumentation.ts
+++ apps/nextjs/src/instrumentation.ts
@@ -1,25 +1,30 @@
-import _ from "lodash"
+// import _ from "lodash"
+// import apm from "elastic-apm-node"
 
 
 export const register = async () => {
-	console.log(process.env)
-	const apm = await import('elastic-apm-node')
-	if(!_.isEmpty(apm) && !apm.isStarted()){
+	// console.log({ apm })
+	if(process.env.NEXT_RUNTIME === 'nodejs'){
+		// console.log({ apm })
+		/* if(!_.isEmpty(apm) && !apm.isStarted()){ */
 		console.log('starting apm')
-		const serviceName = process.env.APP || 'next'
-		apm.start({
-			serviceName:                `server-${serviceName}`,
-			active:                     process.env.APM_IS_ACTIVE === 'true',
-			serverUrl:                  process.env.APM_URL || 'http://localhost:8200',
-			secretToken:                process.env.APM_SECRET_TOKEN || undefined,
-			captureBody:                'all',
-			usePathAsTransactionName:   true,
-			ignoreUrls:                 [/\/_next.*/g],
-			transactionIgnoreUrls:      ["/_next"],
-			captureHeaders:             true,
-			captureErrorLogStackTraces: "always",
-			environment:                process.env.ENVIRONMENT || 'development',
-		})
+		await import("./apm-start.js")
+		// console.log({ apm })
+		// const serviceName = process.env.APP || 'next'
+		// apm.start({
+		// 	serviceName:                `server-${serviceName}`,
+		// 	active:                     process.env.APM_IS_ACTIVE === 'true',
+		// 	serverUrl:                  process.env.APM_URL || 'http://localhost:8200',
+		// 	secretToken:                process.env.APM_SECRET_TOKEN || undefined,
+		// 	captureBody:                'all',
+		// 	usePathAsTransactionName:   true,
+		// 	ignoreUrls:                 [/\/_next.*/g],
+		// 	transactionIgnoreUrls:      ["/_next"],
+		// 	captureHeaders:             true,
+		// 	captureErrorLogStackTraces: "always",
+		// 	environment:                process.env.ENVIRONMENT || 'development',
+		// })
 		console.log('apm started')
+		// }
 	}
 }
